e75516133c4d9cb39c61bf2b7b91c88f02940617
Ohmanitsmebruh <65910726+Ohmanitsmebruh@users.noreply.github.com>
2023-05-24 01:53:37 +0300

    Final commit

--- Script.py
+++ Script.py
@@ -10,12 +10,14 @@ def grad_f(x, y):
 
 
 
-def descente(Points, lamb=[0.001,0.002,0.003,0.004,0.005], eps=1e-10, maxIter=2000):
+def descente(Points, lamb=[0.001,0.002,0.003,0.004,0.005], eps=1e-5, maxIter=2000):
     minValue = -0.1510416
     error = np.array([0.0002, 0.0005, 0.001])
     opt = np.empty((0, 3))
     tempi = []
+    count = 1
     for xy in Points:
+        print("Point " + str(count)+ "\n")
         tempopt = np.array([])
         i = 0
         x = xy[0]
@@ -32,25 +34,30 @@ def descente(Points, lamb=[0.001,0.002,0.003,0.004,0.005], eps=1e-10, maxIter=20
             y = y - lamb[0] * grady
             i += 1
             total = f(x , y)
-            Optimizationgap = total - minValue
-            
-            if Optimizationgap <= error[0] and not ch1:
+            Optimalitygap = total - minValue
+            print("i=%s  x=%.7f  y=%.7f  gradx=%.7f  grady=%.7f Total=%.7f Optim.Gap=%.7f" % (i, x, y, gradx, grady, total, Optimalitygap))
+            if Optimalitygap <= error[0] and not ch1:
                 tempopt = np.append(tempopt, i)
                 ch1 = True
-            elif Optimizationgap <= error[1] and not ch2:
+            elif Optimalitygap <= error[1] and not ch2:
                 tempopt = np.append(tempopt, i)
                 ch2 = True
-            elif Optimizationgap <= error[2] and not ch3:
+            elif Optimalitygap <= error[2] and not ch3:
                 tempopt = np.append(tempopt, i)
                 ch3 = True
         tempopt = tempopt[::-1]
         tempopt = tempopt.reshape(1, -1)
         opt = np.concatenate((opt, tempopt), axis=0)
+        count += 1
+        print("\n")
 
     
     plt.plot(error, opt[0, :], 'g', label='Point 1')
+    plt.plot(error, opt[0, :], 'g*')
     plt.plot(error, opt[1, :], 'b', label='Point 2')
+    plt.plot(error, opt[1, :], 'b*')
     plt.plot(error, opt[2, :], 'r', label='Point 3')
+    plt.plot(error, opt[2, :], 'r*')
     plt.xlabel('Optimality Gap')
     plt.yscale('log')
     plt.ylabel('Number of iterations')
@@ -78,11 +85,14 @@ def descente(Points, lamb=[0.001,0.002,0.003,0.004,0.005], eps=1e-10, maxIter=20
                     break
             tempi.append(i)
         plt.plot(lamb, tempi)
+        plt.plot(lamb, tempi, 'r*')
         plt.xlabel('Lambda')
         plt.yscale('linear')
         plt.xscale('linear')
         plt.ylabel('Number of iterations')
         plt.title("Point " + "("+ str(xy[0]) + ";" + str(xy[1]) + ") (Lambda vs Number of iterations)" )
+        for test in zip(lamb, tempi):
+            plt.annotate('(%.4f, %.4f)' % test, xy=xy)
         plt.show()
 
 
@@ -99,4 +109,6 @@ Points = [[x, y] for x, y in zip(x_values, y_values)]
 
 descente(Points)
 
+
+print("Repository: https://github.com/Ohmanitsmebruh/Assignment10.git \n")
 input("Execution finished! Press any key to exit.")
